27d26a8121a6946c6746d3c557af5cb5cd8b0844
AmaanTazak <amaantazak24@gmail.com>
2025-01-27 12:31:50 +0530

    Updated Render configuration and environment setup

--- config/puma.rb
+++ config/puma.rb
@@ -1,29 +1,14 @@
-workers ENV.fetch("WEB_CONCURRENCY", 2).to_i
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
 
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 5).to_i
-threads threads_count, threads_count
+port ENV.fetch("PORT") { 3000 }
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-# Specifies the `environment` Puma will run in.
-environment ENV.fetch("RAILS_ENV", "development")
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 preload_app!
 
-# Allow Puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
-
-# Run the Solid Queue supervisor inside of Puma for single-server deployments
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
-
-# Specify the PID file.
-pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
-
-# Use a control app for managing Puma (optional but useful for debugging).
-# activate_control_app
-
-on_worker_boot do
-  # Reconnect to the database for each worker (important in clustered mode).
-  ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-end
