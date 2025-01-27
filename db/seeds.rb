b69f5f1c9cd6df77a7e0b3b3ec357c55f72ab021
AmaanTazak <amaantazak24@gmail.com>
2025-01-27 16:10:13 +0530

    admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 # db/seeds.rb
 
-# Create an admin user with a default email and password
-admin = User.create!(
-  email: "admin@gmail.com",
-  password: "admin123",
-  password_confirmation: "admin123",
-  role: "admin"
-)
-
-puts "Admin user created with email: admin@gmail.com and password: admin123"
+# Only create an admin if there are no users
+unless User.exists?
+  admin = User.create!(
+    email: 'admin@example.com', 
+    password: 'admin123', 
+    password_confirmation: 'admin123',
+    role: 'admin'
+  )
+  puts "Admin user created with email: admin@example.com and password: admin123"
+else
+  puts "Admin already exists."
+end
