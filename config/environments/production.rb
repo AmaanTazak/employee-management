84015b446eb07f7abe3b708a3447b825a5bc3f3d
AmaanTazak <amaantazak24@gmail.com>
2025-01-27 13:15:44 +0530

    Cache disable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  
- config.cache_classes = true
+
 config.eager_load = true
 config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 config.assets.compile = false
